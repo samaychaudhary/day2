34005c18ec4a6f72faeacb0a2359b91173c1dc81
“Samay <chaudharysamay98@gmail.com>
2022-07-04 18:35:44 +0545

    :bug: Fixing error

---
+++ day2.py
@@ -0,0 +1,12 @@
+#print ('I\'m beautiful')
+
+#print ("First line")
+
+#print ("first line\nSecond line")
+
+#print ("first line\tsecond line")
+
+#print ("sama\bay")
+
+print ("this is beautiful /\\/\\/\\ mountain")
+
